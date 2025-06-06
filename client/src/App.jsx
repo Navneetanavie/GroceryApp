2055551574f577a431d084713cdc44c57ba49600
Navneeta <nav@Navneetas-MacBook-Air.local>
2025-06-06 19:00:58 +0530

    Completed Buyer Side Web Application

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,26 +8,34 @@ import { useAppContext } from './context/AppContext';
 import Login from './components/Login';
 import AllProducts from './pages/AllProducts';
 import ProductCategory from './pages/ProductCategory';
+import ProductDetails from './pages/ProductDetails';
+import Cart from './pages/Cart';
+import AddAddress from './pages/AddAddress';
+import MyOrders from './pages/MyOrders';
 
 
 
 const App = () => {
 
   const isSellerPath = useLocation().pathname.includes("seller");
-  const {showUserLogin} =useAppContext()
+  const { showUserLogin } = useAppContext()
 
   return (
 
     <div>
-      
+
       {isSellerPath ? null : <Navbar />}
-       {showUserLogin ?  <Login/> : null }
+      {showUserLogin ? <Login /> : null}
       <Toaster />
       <div className={isSellerPath ? "" : 'px-6 md:px-16 lg:px-24 x1:px-32'}>
         <Routes>
           <Route path='/' element={<Home />} />
           <Route path='/products' element={<AllProducts />} />
           <Route path='/products/:category' element={<ProductCategory />} />
+          <Route path='/products/:category/:id' element={<ProductDetails />} />
+          <Route path='/cart' element={<Cart />} />
+          <Route path='/add-address' element={<AddAddress />} />
+          <Route path='/my-orders' element={<MyOrders />} />
         </Routes>
       </div>
       {!isSellerPath && <Footer />}
