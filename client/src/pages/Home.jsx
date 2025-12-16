bb294375ed52cd03f199f5965f078fe20c0843eb
Navneeta <nav@Navneetas-MacBook-Air.local>
2025-12-16 14:05:43 +0530

    Chore/File Name Changed

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -3,7 +3,7 @@ import MainBanner from '../components/MainBanner'
 import Categories from '../components/Categories'
 import BestSeller from '../components/BestSeller'
 import BottomBanner from '../components/BottomBanner'
-import NewsLetter from '../components/NewsLetter'
+import NewsLetter from '../components/NewsLetter.jsx'
 
 const Home = () => {
   return (
